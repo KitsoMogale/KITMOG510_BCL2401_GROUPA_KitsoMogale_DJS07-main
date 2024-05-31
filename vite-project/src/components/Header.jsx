95f5d484282947f9c578f65f476679b2205e3112
Kitso Mogale <kitsomogale19@gmail.com>
2024-05-31 15:45:28 +0200

    meme-generator

---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header () {
+    return (
+      <header className="header">
+         <img src="./Images/Troll-Face.jpg" />
+         <h2> Header Components</h2>
+        <h4>React Course - Project 3</h4>
+     </header>
+    )
+}
